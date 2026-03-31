7e5c49faf50bbcd967b02b8e779128283ad590e2
Alexander <alexkage4@gmail.com>
2026-03-31 10:46:26 -0500

    Initial Commit

---
+++ MareasIA/MareasIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MareasIAApp.swift
+//  MareasIA
+//
+//  Created by Alexander Rojas on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MareasIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
